72c66003f591f6a627e74c1e0156c3f58b3c760c
Angelique Le Roux <angelique.leroux_stg@cooperl.com>
2020-04-08 11:58:15 +0200

    début dev

--- src/com/eni/encheres/dal/articles/ArticleDAO.java
+++
@@ -1,8 +0,0 @@
-package com.eni.encheres.dal.articles;
-
-import com.eni.encheres.bo.ArticleVendu;
-import com.eni.encheres.dal.exceptions.ArticleDAOException;
-
-public interface ArticleDAO {
-    void insert(ArticleVendu article) throws ArticleDAOException;
-}
